e8c2cc6166fc9842972c2a096bc1f0965ba8094c
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-12-21 12:10:41 +0300

    init project

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="classic",
+    version="0.0.1",
+    description="Classic ML algorithms",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
